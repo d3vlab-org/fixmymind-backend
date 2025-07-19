abea27ca73c199c8da36c31047d159bbda9155b9
d3vlab-org <drabek.jakub@icloud.com>
2025-07-19 04:19:53 +0200

    Handle missing .env and run built-in server

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Skrypt startowy obsługujący php-fpm oraz artisan serve w zależności od APP_ROLE
 
 # Utwórz katalogi dla logów
 mkdir -p /var/log/supervisor
@@ -12,6 +13,25 @@ php artisan view:cache
 chown -R www-data:www-data /var/www/html/storage
 chown -R www-data:www-data /var/www/html/bootstrap/cache
 
-
-# Uruchom Supervisor z wygenerowaną konfiguracją
-exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+# W zależności od roli uruchom odpowiednią usługę
+case "$APP_ROLE" in
+    "web")
+        exec php artisan serve --host=0.0.0.0 --port=8080
+        ;;
+    "dev")
+        exec php artisan serve --host=0.0.0.0 --port=8080
+        ;;
+    "fpm")
+        exec php-fpm
+        ;;
+    "test")
+        exec php artisan test
+        ;;
+    "worker")
+        exec php artisan queue:work --sleep=3 --tries=3 --max-time=3600
+        ;;
+    "all"|*)
+        # Domyślnie uruchamiamy cały zestaw procesów przez Supervisora
+        exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+        ;;
+esac
